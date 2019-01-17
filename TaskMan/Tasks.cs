4952bc3524d40bc7d1a8f2c366100297f3ee06d3
yanix <yanix@DESKTOP-VTA0RSL>
2019-01-17 03:33:47 +0200

    Add project files.

---
+++ TaskMan/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMan
+{
+    class Tasks
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string CPU { get; set; }
+        public string Memory { get; set; }
+        public string Disk { get; set; }
+
+    }
+}
---
+++ TaskManagerWF/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManagerWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Process[] proc;
+        public void GetAllProc()
+        {
+            proc = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process p in proc)
+            {
+                listBox1.Items.Add(p.ProcessName);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllProc();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            proc[listBox1.SelectedIndex].Kill();
+            proc[listBox1.SelectedIndex].
+        }
+    }
+}
